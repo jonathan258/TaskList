79b0414f337f9c1b46ddbbca28201b1b51068744
jonathan <campbelljon620@gmail.com>
2025-03-08 19:36:20 -0400

    Created the application to accept create and also some validation if it is blank it shows red error and if the form is filled out it shows that it has been filled out successfully

--- task-list/resources/views/layouts/app.blade.php
+++ task-list/resources/views/layouts/app.blade.php
@@ -2,10 +2,15 @@
 <html>
     <head>
         <title>Task list</title>
+        @yield('styles')
     </head>
     <body>
         <h1>@yield('title')</h1>
         <div>
+            @if (session()->has('success'))
+            <div>{{session('success')}}</div>
+        </div>
+            @endif
        @yield('content')
         </div>
     </body>
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use \App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
@@ -13,7 +13,7 @@ Route::get('/', function () {
 //Route to show all task
 Route::get('/tasks', function () {
     return view('index',[
-         'tasks' => \App\Models\Task::latest()->get()
+         'tasks' => Task::latest()->get()
 ]);
 })->name('tasks.index');
 
@@ -23,10 +23,24 @@ Route::view('/tasks/create','create')
 
 //Route to show a single task
 Route::get('/{id}', function($id)  {
-    return view('show',['task' => \App\Models\Task::findOrFail($id)]);
+    return view('show',['task' => Task::findOrFail($id)]);
 
 })->name('task.show');
 
 Route::post('/tasks',function(Request $request) {
-    dd($request->all());
+   $data= $request ->validate([
+    'title'=> 'required|max:255',
+    'description'=> 'required',
+    'long_description'=>'required'
+   ]);
+
+   $task = new Task;
+   $task -> title = $data['title'];
+   $task -> description = $data['description'];
+   $task -> long_description = $data['long_description'];
+
+   $task ->save();
+
+   return redirect() -> route('task.show',['id'=> $task->id])
+   ->with('success','Task created successfully!') ;
 })->name('tasks.store');
